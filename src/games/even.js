62329af5dacf4ae88c0b1ca26034e063fdcdfb19
kamil <kamilxai009@gmail.com>
2024-02-14 12:16:48 +0300

    gugagaga

--- src/games/even.js
+++ src/games/even.js
@@ -1,36 +1,42 @@
 import readlineSync from 'readline-sync';
 
 const isEven = (getRandomNumber) => {
-  return getRandomNumber % 2 === 0; 
+  return getRandomNumber % 2 === 0;
 };
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 101);
+}
+
 
 export const b = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 
-  const  getRandomNumber = () => {
-    return Math.floor(Math.random() * 101);
-  }
-  
-  let num = getRandomNumber();
-
   console.log('Answer "yes" if the number is even, otherwise answer "no".')
-  console.log(`Question: ${num}`)
 
-  const answer = readlineSync.question('Your answer: ')
+  for (let i = 1; i <= 3; i++) {
 
-  let correctAnswers = (isEven(num)) ? 'yes' : 'no';
+    const numb = getRandomNumber();
+
+    console.log(`Question: ${numb}`);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    const correctAnswers = isEven(numb) ? 'yes' : 'no';
+
+    if (answer === correctAnswers) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer. Correct answer was '${correctAnswers}'.`);
+      console.log(`Let's try again!`);
+      break;
+    }
+  };
 
 
-  if (answer === correctAnswers) {
-    console.log('Correct!')
-  } else {
-    console.log(`'${answer}' is wrong answer . Correct answer was '${correctAnswers}'.`);
-    console.log(`Let's try again,!`);
-    process.exit(0)
-  }
-  return (b);
+  return b;
 };
 
 
