279bb698fd50cba4f049efad19515741a0ce669e
vlad <vladtyrig@mail.ru>
2024-06-03 13:09:58 +0300

    add src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -6,5 +6,5 @@ export const gg = () => {
 console.log('brain-games')
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
-console.log(`hello, ${name}!`);
+console.log(`Hello, ${name}`);
 };
